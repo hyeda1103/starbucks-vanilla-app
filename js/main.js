862455f596e571bff740891781e58814d8bfc524
dahyeko <dalgona92@sungshin.ac.kr>
2021-03-23 19:35:15 +0900

    Add slides

--- js/main.js
+++ js/main.js
@@ -14,3 +14,58 @@ searchInputEl.addEventListener('blur', function() {
     searchEl.classList.remove('focused')
     searchInputEl.setAttribute('placeholder', '')
 }) 
+
+const badgeEl = document.querySelector('header .badges')
+
+window.addEventListener('scroll', _.throttle(function(){
+    if(window.scrollY > 500) {
+        // 뱃지 숨기기
+        // gsap.to(요소, 지속시간, 옵션)
+        gsap.to(badgeEl, .6, {
+            opacity: 0,
+            display: 'none'
+        });
+    } else {
+        // 뱃지 보이기
+        gsap.to(badgeEl, .6, {
+            opacity: 1,
+            display: 'block'
+        });
+    }
+}, 300))
+
+const fadeEls = document.querySelectorAll('.visual .fade-in')
+
+fadeEls.forEach((fadeEl, index) => {
+    // gsap.to(요소, 지속시간, 옵션)
+    gsap.to(fadeEl, 1, {
+        delay: (index + 1) * .7,
+        opacity: 1
+    })
+})
+
+// new Swiper(선택자, 옵션)
+new Swiper('.notice-line .swiper-container', {
+    direction: 'vertical',
+    autoplay: true,
+    loop: true
+})
+
+new Swiper('.promotion .swiper-container', {
+    slidesPerView: 3, // 한번에 보여주는 슬라이드 개수
+    spaceBetween: 10, // 슬라이드 사이 간격
+    centeredSlides: true, // 1번 슬라이드가 가운데 보이기
+    loop: true,
+    autoplay: {
+        delay: 5000 // 5초에 한번씩 자동 슬라이드
+    },
+    pagination: {
+        el: '.promotion .swiper-pagination', // 페이지 번호 요소 선택자
+        clickable: true
+    },
+    navigation: {
+        prevEl: '.promotion .swiper-prev',
+        nextEl: '.promotion .swiper-next'
+    }
+})
+
